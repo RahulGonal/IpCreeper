563f2eedda1a79528f1f7fae01f0d1e1ec0214e4
RahulGonal <76273611+RahulGonal@users.noreply.github.com>
2021-05-22 16:20:00 +0530

    Update IpCreeper.py

--- IpCreeper.py
+++ IpCreeper.py
@@ -2,17 +2,19 @@ import ipinfo
 from pprint import pprint
 import time
 
-print("Welcome to IP Creeper")
-time.sleep(2)
-print("The Creator of the tool is not responsible for any misuse of this tool")
-time.sleep(2)
-print("This tool is meant for educational purposes only")
-time.sleep(3)
 print("Ｉｐ Ｃｒｅｅｐｅｒ")
 print("Created by RahulGonal")
 time.sleep(5)
+print("Welcome to IP Creeper")
+time.sleep(5)
+print("The Creator of the tool is not responsible for any misuse of this tool")
+time.sleep(5)
+print("This tool is meant for educational purposes only")
+time.sleep(5)
+
 
 print("You need access token to continue, sign in to ipinfo.io to get access token")
+time.sleep(5)
 access_token = input('Type your access token from ipinfo.io >>>>> ')
 handler = ipinfo.getHandler(access_token)
 ip_address = input('Enter the IP Address of the victim >>>>> ')
