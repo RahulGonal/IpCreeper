0f3a71c97e4140188c0dcf5a0b79f88fa78c224d
RahulGonal <76273611+RahulGonal@users.noreply.github.com>
2021-06-26 17:42:18 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(name="IpCreeper",
+version="1.0",
+author="RahulGonal"
+author_email="mynameisrahul.gonal@gmail.com"
+url='https://github.com/RahulGonal/IpCreeper'
+license='"MIT License"
+install_requires=[
+        'ipinfo',
+        'faker'
+    ]
+)
