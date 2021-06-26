d4e9c0bb26fa2582e99e132685b5bb80878060d8
RahulGonal <76273611+RahulGonal@users.noreply.github.com>
2021-06-26 10:22:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name="IpCreeper",
     version="1.0",
     author="RahulGonal"
-    author_email="mynameisrahul.gonal@gmail.com"
     url='https://github.com/RahulGonal/IpCreeper'
     license='"MIT License"
     install_requires=[
