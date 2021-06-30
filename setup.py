bb75da8f7a1826fe735e0578ff05818431818445
RahulGonal <76273611+RahulGonal@users.noreply.github.com>
2021-06-30 11:59:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name="IpCreeper-Setup",
+setup(name="IpCreeper",
 version="1.0",
 author="RahulGonal",
 author_email="mynameisrahul.gonal@gmail.com",
